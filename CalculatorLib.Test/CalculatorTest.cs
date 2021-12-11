029e4d4f9273da86b44b0a61424b21c0985cc8e6
Starinin Andrey <starininandrey@gmail.com>
2021-12-11 14:09:21 +0300

    Add unit tests

--- CalculatorLib.Test/CalculatorTest.cs
+++ CalculatorLib.Test/CalculatorTest.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CalculatorLib.Test
@@ -6,8 +7,61 @@ namespace CalculatorLib.Test
     public class CalculatorTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Add_5_10_15()
         {
+            int x = 5;
+            int y = 10;
+            double expected = 15;
+
+            var actual = Calculator.Add(x, y);
+            
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Sub_10_5_5()
+        {
+            int x = 10;
+            int y = 5;
+            double expected = 5;
+
+            var actual = Calculator.Sub(x, y);
+            
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [TestMethod]
+        public void Mult_10_5_50()
+        {
+            int x = 10;
+            int y = 5;
+            double expected = 50;
+
+            var actual = Calculator.Mult(x, y);
+            
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [TestMethod]
+        public void Div_10_5_2()
+        {
+            int x = 10;
+            int y = 5;
+            double expected = 2;
+
+            var actual = Calculator.Div(x, y);
+            
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException), "Error")]
+        public void Div_Exception()
+        {
+            int x = 10;
+            int y = 0;
+
+            Calculator.Div(x, y);
         }
     }
 }

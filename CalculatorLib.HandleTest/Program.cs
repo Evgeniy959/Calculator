f9eb440637b0f74cd5a490d9c4f93ff7e108368b
Starinin Andrey <starininandrey@gmail.com>
2021-12-11 13:37:16 +0300

    Add test

--- CalculatorLib.HandleTest/Program.cs
+++ CalculatorLib.HandleTest/Program.cs
@@ -4,9 +4,36 @@ namespace CalculatorLib.HandleTest
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            int x = 5;
+            int y = 10;
+            int y_error = 0;
+            double res = 0;
+
+            res = Calculator.Add(x, y);
+            Console.WriteLine($"{x} + {y} = {res}");
+            
+            res = Calculator.Sub(x, y);
+            Console.WriteLine($"{x} - {y} = {res}");
+            
+            res = Calculator.Mult(x, y);
+            Console.WriteLine($"{x} * {y} = {res}");
+            
+            res = Calculator.Div(x, y);
+            Console.WriteLine($"{x} / {y} = {res}");
+
+            try
+            {
+                res = Calculator.Add(x, y_error);
+                Console.WriteLine($"{x} + {y} = {res}");
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(e.Message);
+                Console.ResetColor();
+            }
         }
     }
 }

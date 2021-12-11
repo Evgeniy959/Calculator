7a647223585133e83bb2b766d0738a5ffb21b1a3
Starinin Andrey <starininandrey@gmail.com>
2021-12-11 13:30:34 +0300

    Init proj

---
+++ CalculatorLib/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace CalculatorLib
+{
+    public static class Calculator
+    {
+        public static double Add(double x, double y) => x + y;
+        public static double Sub(double x, double y) => x - y;
+        public static double Mult(double x, double y) => x * y;
+
+        public static double Div(double x, double y)
+        {
+            if (y == 0)
+            {
+                throw new DivideByZeroException("Делить на ноль нельзя!");
+            }
+
+            return x / y;
+        }
+    }
+}

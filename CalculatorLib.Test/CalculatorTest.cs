19c29b2653d94392f92836246d3e4a4cd87f30cf
Starinin Andrey <starininandrey@gmail.com>
2021-12-11 13:43:21 +0300

    Add UnitTest

---
+++ CalculatorLib.Test/CalculatorTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculatorLib.Test
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
